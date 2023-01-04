138e1974f6c5acb0b29f1eee1b75c0f3d69d9464
gregvansci <gregvansci76@hotmail.com>
2023-01-03 18:33:50 -0800

    Added UI components as objects

--- app/src/main/java/com/gregvansci/distancetracker/MainActivity.java
+++ app/src/main/java/com/gregvansci/distancetracker/MainActivity.java
@@ -3,12 +3,28 @@ package com.gregvansci.distancetracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // references to the UI elements
+
+    EditText distanceTravelled;
+    TextView units;
+    Button startTracking;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // give each UI variable a value
+        distanceTravelled = findViewById(R.id.distanceTravelled);
+        units = findViewById(R.id.units);
+        startTracking = findViewById(R.id.startTracking);
+
+        distanceTravelled.setText("0");
     }
 }
