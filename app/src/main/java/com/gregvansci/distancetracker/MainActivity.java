271d629ed02fc0ea618084cc91afc21fb6177408
gregvansci <gregvansci76@hotmail.com>
2023-01-05 18:28:54 -0800

    Added addDistance function

--- app/src/main/java/com/gregvansci/distancetracker/MainActivity.java
+++ app/src/main/java/com/gregvansci/distancetracker/MainActivity.java
@@ -3,6 +3,7 @@ package com.gregvansci.distancetracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     TextView units;
     Button startTracking;
 
+    float distanceTravelledMeters;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +28,37 @@ public class MainActivity extends AppCompatActivity {
         units = findViewById(R.id.units);
         startTracking = findViewById(R.id.startTracking);
 
-        distanceTravelled.setText("0");
+        distanceTravelledMeters = 0;
+        distanceTravelled.setText(String.valueOf(distanceTravelledMeters));
+
+        startTracking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addDistance(11.11f);
+            }
+        });
+    }
+
+    private void startTracking() {
+        distanceTravelledMeters += 1;
+    }
+
+    private void stopTracking() {
+
+    }
+
+    private void addDistance(float val) {
+        distanceTravelledMeters += val;
+        String distanceTravelledString;
+        if (distanceTravelledMeters > 1000) {
+            distanceTravelledString = String.valueOf(distanceTravelledMeters / 1000);
+            units.setText("kilometers");
+        }
+        else if (distanceTravelledMeters > 100) {
+            distanceTravelledString = String.valueOf((int)distanceTravelledMeters);
+        } else {
+            distanceTravelledString = String.valueOf(distanceTravelledMeters);
+        }
+        distanceTravelled.setText(distanceTravelledString);
     }
 }
