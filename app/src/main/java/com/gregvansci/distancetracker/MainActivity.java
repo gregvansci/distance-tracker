2b1e7722c8b8cc8f3507eb95dbace1bd37c95db5
gregvansci <gregvansci76@hotmail.com>
2023-01-06 16:47:26 -0800

    Added ScheduledExecutorService that toggles with button

--- app/src/main/java/com/gregvansci/distancetracker/MainActivity.java
+++ app/src/main/java/com/gregvansci/distancetracker/MainActivity.java
@@ -8,15 +8,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
 
     // references to the UI elements
 
     EditText distanceTravelled;
     TextView units;
-    Button startTracking;
+    Button toggleTracking;
 
     float distanceTravelledMeters;
+    boolean tracking;
+
+    ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+    ScheduledFuture<?> scheduledFuture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,26 +35,50 @@ public class MainActivity extends AppCompatActivity {
         // give each UI variable a value
         distanceTravelled = findViewById(R.id.distanceTravelled);
         units = findViewById(R.id.units);
-        startTracking = findViewById(R.id.startTracking);
+        toggleTracking = findViewById(R.id.toggleTracking);
 
         distanceTravelledMeters = 0;
         distanceTravelled.setText(String.valueOf(distanceTravelledMeters));
 
-        startTracking.setOnClickListener(new View.OnClickListener() {
+        toggleTracking.setText("Start Tracking");
+
+        toggleTracking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addDistance(11.11f);
+                if (tracking) {
+                    toggleTracking.setText("Start Tracking");
+                    tracking = false;
+                    stopTracking();
+                } else {
+                    toggleTracking.setText("Stop Tracking");
+                    tracking = true;
+                    startTracking();
+                }
             }
         });
     }
 
     private void startTracking() {
-        distanceTravelledMeters += 1;
+        // start tracking
+        // get the current location and save it to a variable
+
+        // start the executor service to run every second and find the distance between the current location and the last location
+        scheduledFuture = scheduledExecutorService.scheduleAtFixedRate(updateDistance, 250, 250, TimeUnit.MILLISECONDS);
     }
 
     private void stopTracking() {
-
+        // stop tracking
+        if (scheduledFuture != null) {
+            scheduledFuture.cancel(true);
+        }
     }
+
+    Runnable updateDistance = new Runnable() {
+        @Override
+        public void run() {
+            addDistance(11.11f);
+        }
+    };
 
     private void addDistance(float val) {
         distanceTravelledMeters += val;
