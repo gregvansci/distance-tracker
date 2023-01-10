ba0e9f7f09b05e4f83ad4a2fd6da7a4e55413ea6
gregvansci <gregvansci76@hotmail.com>
2023-01-09 16:53:13 -0800

    Added permissions request for location information

--- app/src/main/java/com/gregvansci/distancetracker/MainActivity.java
+++ app/src/main/java/com/gregvansci/distancetracker/MainActivity.java
@@ -1,18 +1,37 @@
 package com.gregvansci.distancetracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.location.Location;
+
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_FINE_LOCATION = 99;
 
     // references to the UI elements
 
@@ -25,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
 
     ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
     ScheduledFuture<?> scheduledFuture;
+
+    Location lastLocation, curLocation;
+    LocationRequest locationRequest;
+    LocationCallback locationCallback;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +65,18 @@ public class MainActivity extends AppCompatActivity {
         distanceTravelledMeters = 0;
         distanceTravelled.setText(String.valueOf(distanceTravelledMeters));
 
+        locationRequest = new LocationRequest();
+
         toggleTracking.setText("Start Tracking");
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            System.out.println("Permission granted");
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
+            }
+        }
 
         toggleTracking.setOnClickListener(v -> {
             if (tracking) {
@@ -47,28 +84,52 @@ public class MainActivity extends AppCompatActivity {
                 tracking = false;
                 stopTracking();
             } else {
-                toggleTracking.setText("Stop Tracking");
-                tracking = true;
-                startTracking();
+                if (startTracking()) {
+                    toggleTracking.setText("Stop Tracking");
+                    tracking = true;
+                }
             }
         });
     }
 
-    private void startTracking() {
+    private boolean startTracking() {
+        // check for permissions
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            System.out.println("No permissions to start tracking");
+//            return false;
+//        }
         // start tracking
         // get the current location and save it to a variable
 
+
         // start the executor service to run every second and find the distance between the current location and the last location
         scheduledFuture = scheduledExecutorService.scheduleAtFixedRate(updateDistance, 666, 666, TimeUnit.MILLISECONDS);
+        return true;
     }
 
     private void stopTracking() {
+
         // stop tracking
         if (scheduledFuture != null) {
             scheduledFuture.cancel(true);
         }
         // find the distance between the current location and the last location
         addDistance(11.12f);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSIONS_FINE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    return;
+                } else {
+                    Toast.makeText(this, "This app requires permissions to be granted in order to work properly", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+        }
     }
 
     Runnable updateDistance = () -> addDistance(11.11f);
